fcfda10dd3da8affc9b5a7c2bfa9153b39f5c614
W I L L <will@gravina.dev>
2025-07-26 09:18:46 -0300

    chore: update package.json and add Vitest configuration for testing and coverage

---
+++ vitest.config.ts
@@ -0,0 +1,13 @@
+import { coverageConfigDefaults, defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    projects: ['apps/*', 'packages/*', 'configs/*'],
+    coverage: {
+      reporter: ['lcov', 'html'],
+      all: true,
+      provider: 'v8',
+      exclude: [...coverageConfigDefaults.exclude, '**/dist/**', '**/coverage/**', '**/fixtures/**', '**/tests/**', './turbo/**', './scripts/**']
+    }
+  }
+})
