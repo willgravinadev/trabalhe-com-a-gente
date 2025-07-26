3385f83232aa192cdb6fbac7972a2a342451eebb
W I L L <will@gravina.dev>
2025-07-26 19:54:12 -0300

    feat: add API package with TypeScript, ESLint configuration, and essential dependencies

---
+++ apps/api/eslint.config.mjs
@@ -0,0 +1,18 @@
+import eslintConfig from '@github-search/eslint-config'
+
+export default eslintConfig(
+  {
+    project: './tsconfig.json',
+    tsconfigRootDir: import.meta.dirname,
+    turbo: true,
+    typescript: true
+  },
+  {
+    rules: {
+      '@typescript-eslint/no-unsafe-return': 'off',
+      'sonarjs/no-async-constructor': 'off',
+      '@typescript-eslint/no-unsafe-argument': 'off',
+      '@typescript-eslint/no-redundant-type-constituents': 'off'
+    }
+  }
+)
