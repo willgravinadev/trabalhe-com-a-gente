c5cb4699da820c6d3f46032c3eab2e0c45e9f14c
W I L L <will@gravina.dev>
2025-07-27 23:19:37 -0300

    chore: update ESLint configuration to disable additional TypeScript rules for improved linting flexibility

--- apps/api/eslint.config.mjs
+++ apps/api/eslint.config.mjs
@@ -12,7 +12,8 @@ export default eslintConfig(
       '@typescript-eslint/no-unsafe-return': 'off',
       'sonarjs/no-async-constructor': 'off',
       '@typescript-eslint/no-unsafe-argument': 'off',
-      '@typescript-eslint/no-redundant-type-constituents': 'off'
+      '@typescript-eslint/no-redundant-type-constituents': 'off',
+      '@typescript-eslint/unbound-method': 'off'
     }
   }
 )
